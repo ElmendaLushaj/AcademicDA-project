6a72cf30f62d97ca8da492985bf1f1ed9e842a36
ElmendaLushaj <63049261+ElmendaLushaj@users.noreply.github.com>
2021-01-08 00:11:17 +0100

    helpers

---
+++ src/main/java/com/example/demo/Helpers/ProfessorResponse.java
@@ -0,0 +1,21 @@
+package com.example.demo.Helpers;
+
+
+
+import com.example.demo.Model.Professor;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ProfessorResponse implements Serializable {
+    private Professor professor;
+
+    public
+
+    ProfessorResponse(Professor professor) {
+        this.professor = professor;
+    }
+
+
+}
