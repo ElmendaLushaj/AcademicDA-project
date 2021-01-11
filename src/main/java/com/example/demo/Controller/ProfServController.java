f38f8676c24f1a4d7acfe2706a2d931cec33b419
MelinaLushaku <63053349+MelinaLushaku@users.noreply.github.com>
2021-01-11 23:39:06 +0100

    getAndDeleteObjectsById

--- src/main/java/com/example/demo/Controller/ProfServController.java
+++ src/main/java/com/example/demo/Controller/ProfServController.java
@@ -2,10 +2,9 @@ package com.example.demo.Controller;
 
 import com.example.demo.Helpers.FolderHelper;
 import com.example.demo.Helpers.RegisterHelper;
+import com.example.demo.Helpers.getModelHelper;
 import com.example.demo.Helpers.saveDocumentHelper;
-import com.example.demo.Model.Document;
-import com.example.demo.Model.Folder;
-import com.example.demo.Model.Professor;
+import com.example.demo.Model.*;
 import com.example.demo.Services.IProfessorService;
 import com.example.demo.Services.IUserInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping( "/api/professor")
@@ -27,16 +27,56 @@ public class ProfServController {
 
     }
     @PostMapping("/addDoc")
-    public void register(@RequestBody saveDocumentHelper sdH){
+    public void addDoc(@RequestBody saveDocumentHelper sdH){
 
 
         Document d = new Document(sdH.getCreationD() , sdH.getDocPath() , sdH.getEditedD(), sdH.getFileSize(),sdH.getName(),sdH.getType(),sdH.getProfID());
         profServ.addDocument(d);
+    }
+    @PostMapping("/deleteDoc")
+    public void deleteDoc(@RequestBody getModelHelper gtm){
+         profServ.deleteDoc(gtm.getModelId());
     }
 
     @PostMapping("/addFolder")
     public  void addFolder(@RequestBody FolderHelper folderh){
         Folder f=new Folder( folderh.getName(),folderh.getProfessorID());
         profServ.addFolder(f);
+    }
+
+    @PostMapping("/deleteFold")
+    public void deleteFold(@RequestBody getModelHelper gtm){
+        profServ.deleteFold(gtm.getModelId());
+    }
+
+    @GetMapping("/getDoc")
+    public ResponseEntity getDocument(@RequestBody getModelHelper gtm){
+
+        Optional<Document> d =this.profServ.getDocById(gtm.getModelId());
+        return ResponseEntity.ok(d);
+
+    }
+    @GetMapping("/getFold")
+    public ResponseEntity getFolder(@RequestBody getModelHelper gtm){
+
+        Optional<Folder> f =this.profServ.getFoldById(gtm.getModelId());
+        return ResponseEntity.ok(f);
+
+    }
+
+    @GetMapping("/getComm")
+    public ResponseEntity getComment(@RequestBody getModelHelper gtm){
+
+        Optional<Comment> c =this.profServ.getCommById(gtm.getModelId());
+        return ResponseEntity.ok(c);
+
+    }
+
+    @GetMapping("/getApprove")
+    public ResponseEntity getApprovement(@RequestBody getModelHelper gtm){
+
+        Optional<Approvement> a =this.profServ.getAppById(gtm.getModelId());
+        return ResponseEntity.ok(a);
+
     }
 }
--- src/main/java/com/example/demo/Services/ProfessorService.java
+++ src/main/java/com/example/demo/Services/ProfessorService.java
@@ -1,34 +1,81 @@
 package com.example.demo.Services;
 
+import com.example.demo.DAL.ApprovementDAO;
+import com.example.demo.DAL.CommentDAO;
 import com.example.demo.DAL.DocumentDAO;
 import com.example.demo.DAL.FolderDao;
+import com.example.demo.Model.Approvement;
+import com.example.demo.Model.Comment;
 import com.example.demo.Model.Document;
 import com.example.demo.Model.Folder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfessorService implements IProfessorService{
     @Autowired
     private DocumentDAO docDao;
     private FolderDao folDao;
+    private CommentDAO commDao;
+    private ApprovementDAO appDao;
+    private Document doc;
     @Override
     public List<Document> getAllDocuments() {
+
         return this.docDao.findAll();
     }
 
     @Override
     public void addDocument(Document d) {
+
         docDao.save(d);
     }
     @Override
     public  List<Folder>getAllFolders(){
         return this.folDao.findAll();
     }
+
     @Override
     public void addFolder(Folder d) {
+
         folDao.save(d);
+    }
+
+    @Override
+    public Optional<Document> getDocById(int docId){
+       Optional<Document> d= docDao.findById(docId);
+       return d;
+
+    }
+
+    @Override
+    public Optional<Folder> getFoldById(int folId){
+        Optional<Folder> f= folDao.findById(folId);
+        return f;
+
+    }
+    @Override
+    public Optional<Comment> getCommById(int comId){
+        Optional<Comment> c= commDao.findById(comId);
+        return c;
+
+    }
+
+    @Override
+    public Optional<Approvement> getAppById(int appId){
+        Optional<Approvement> a= appDao.findById(appId);
+        return a;
+
+    }
+    @Override
+    public void deleteDoc(int id){
+        docDao.deleteById(id);
+    }
+    @Override
+    public void deleteFold(int id){
+        folDao.deleteById(id);
     }
 }
