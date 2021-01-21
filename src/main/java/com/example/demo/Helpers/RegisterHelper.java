005c84e2a137e9a7aefcc5de9caa7c6de112ae0d
KujteseLahi <kl43371@ubt-uni.net>
2021-01-21 01:14:40 +0100

    commit1

--- src/main/java/com/example/demo/Helpers/RegisterHelper.java
+++ src/main/java/com/example/demo/Helpers/RegisterHelper.java
@@ -10,17 +10,22 @@ public class RegisterHelper implements Serializable {
     private String username;
     private String password;
 
-    public RegisterHelper(String name, String degree, String email, String password, String username) {
+    /* public RegisterHelper(String name, String degree, String email, String password, String username) {
 
-        this.name=name;
-       this.email=email;
-       this.degree = degree;
-       this.username = username;
-       this.password = password;
+         this.name=name;
+        this.email=email;
+        this.degree = degree;
+        this.username = username;
+        this.password = password;
+     }
+   */
+    public RegisterHelper(RegisterHelper.RegisterHelperBuilder registerHelperBuilder) {
+        this.name = registerHelperBuilder.name;
+        this.email = registerHelperBuilder.email;
+        this.degree = registerHelperBuilder.degree;
+        this.username = registerHelperBuilder.username;
+        this.password = registerHelperBuilder.password;
     }
-
-
-
     public String getName() {
         return name;
     }
@@ -59,5 +64,35 @@ public class RegisterHelper implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public static class RegisterHelperBuilder{
+        private String name;
+        private String email;
+        private String degree;
+        private String username;
+        private String password;
+
+        public  RegisterHelperBuilder(String username,String password){
+            this.username=username;
+            this.password=password;
+
+        }
+        public RegisterHelper.RegisterHelperBuilder setName(String name){
+            this.name=name;
+            return this;
+        }
+        public RegisterHelper.RegisterHelperBuilder setEmail(String email){
+            this.email=email;
+            return this;
+        }
+        public RegisterHelper.RegisterHelperBuilder setDegree(String degree){
+            this.degree=degree;
+            return this;
+        }
+        public RegisterHelper build(){
+            return  new RegisterHelper(this);
+        }
+
+
     }
 }
